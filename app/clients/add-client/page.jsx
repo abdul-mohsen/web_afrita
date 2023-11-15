5df64641a9fcc8ce91474b53c1820a59229a2c20
Mustafa Khaled <chiiit1754@gmail.com>
2023-11-15 18:37:35 +0200

    add the  missing input

--- app/clients/add-client/page.jsx
+++ app/clients/add-client/page.jsx
@@ -7,19 +7,32 @@ export default function AddClinet() {
   return (
     <div>
         <div className='breadcrumb w-full text-xl flex flex-row gap-5 justify-start items-center mb-6'>
-            <Link href={"/suppliers"} >
+            <Link href={"/clients"} >
                 <HiOutlineUsers className='text-primary' />
 
             </Link>
             <HiChevronDoubleLeft className='text-app-gray' />
-            <h2 className='text-app-gray'>عميل جديد </h2>
+            <h2 className='text-app-gray'> اضافة عميل </h2>
         </div>
         <div className="add-form w-full bg-white p-8 rounded-xl">
             <form class="max-w-[600px]">
-                <h3 className='text-primary text-2xl '> اسم العميل</h3>
+                <h3 className='text-primary text-2xl '>  عميل جديد</h3>
                 <div class="space-y-12">
                     <div class="pb-12">
                             <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-4 border-b border-gray-900/10 pb-12">
+                            <div class="sm:col-span-4">
+                                <label
+                                    for="product-name"
+                                    class="block text-lg font-medium leading-6 text-gray-900">  اسم العميل </label>
+                                <div class="mt-2">
+                                    <input
+                                        type="phone"
+                                        name="product-name"
+                                        id="product-name"
+                                        autocomplete="given-name"
+                                        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6"/>
+                                </div>
+                            </div>
                             <div class="sm:col-span-4">
                                 <label
                                     for="product-name"
