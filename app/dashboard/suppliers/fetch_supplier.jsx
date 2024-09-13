bb26eec3e1072aee30b1ccaa3a1e9bc11834a8b9
abdul-mohsen <abdh357@gmail.com>
2024-09-13 14:30:39 +0300

    test 13

---
+++ app/dashboard/suppliers/fetch_supplier.jsx
@@ -0,0 +1,70 @@
+'use client';
+import { useState, useEffect } from 'react';  // Import useEffect and useState
+import axios from 'axios';
+import { useSession } from 'next-auth/react';
+import { toast } from 'sonner';
+import { useRouter } from 'next/navigation';
+
+const FetchSuppliers = () => {
+
+    const { data: userSession } = useSession();
+    const [suppliers, setSuppliers] = useState(null)
+    useEffect(() => {
+    const fetchInvoices = async () => {
+      try {
+        const response = await axios.get(
+          `${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/v2/parts_provider`,
+          {
+            headers: {
+              Authorization: `Bearer ${userSession?.user?.accessToken}`,
+            },
+          }
+        );
+        setSuppliers(response.data)
+        
+      } catch (error) {
+        console.error('Error fetching invoices:', error);
+      }
+    };
+    fetchInvoices();  
+  }, []);  
+
+  const router = useRouter()
+
+  // To Delete An Invoice
+  const deleteInvoice = async (id) => {
+    try {
+      const response = await axios.delete(
+        `${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/v1/bills/2?store_id=${id}`,
+        {
+          headers: {
+            Authorization: `Bearer ${userSession?.user?.accessToken}`,
+          },
+        }
+      );
+      if (response.ok) {
+        console.log(`Invoice ${id} deleted successfully.`);
+      }
+      toast.success(`Invoice ${id} deleted successfully`)
+      router.push('/dashboard/invoices')
+      if (!response) {
+        const { error } = response
+        toast.error(error)
+      }
+    } catch (error) {
+      toast.error(`Invoice ${id} can't deleted`)
+      router.push('/dashboard/invoices')
+      console.error(`Error deleting invoice with ID ${id}:`, error.message);
+    }
+  };
+
+  return (
+    <>
+            {suppliers.map((item) => {
+                return <SupplierItem {...item} key={item._id}/>
+            })}
+    </>
+  );
+};
+
+export default FetchSuppliers;
