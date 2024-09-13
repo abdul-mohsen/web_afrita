d05a5e69e40dcbb5c39775cab4013399cea3752a
abdul-mohsen <abdh357@gmail.com>
2024-09-13 09:44:30 +0300

    hmm t7

--- app/dashboard/suppliers/page.jsx
+++ app/dashboard/suppliers/page.jsx
@@ -1,3 +1,4 @@
+'use Client'
 import { SectionNav, SectionTags, SupplierItem } from "@/components";
 import DropdownHeading from "@/components/DropdownHeading";
 import PagesNumber from "@/components/PagesNumber";
