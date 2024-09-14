0f7d5e841a1ca8507a25b195f7472509d19ab29d
abdul-mohsen <abdh357@gmail.com>
2024-09-14 18:58:42 +0300

    test 24

--- components/AddSupplierForm.jsx
+++ components/AddSupplierForm.jsx
@@ -1,6 +1,7 @@
 'use client'
 import React, { useEffect } from 'react'
 import { markRequiredInputs } from '@/utils/utils';
+import axios from 'axios';
 
 const AddSupplierForm = () => {
     async function handleAdd(data) {
