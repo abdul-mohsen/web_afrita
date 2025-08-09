6e5731e84fd3253f8f2f26083b6151175fa0df74
ssda <abdh357@gmail.com>
2025-08-09 22:24:58 +0300

    fix:

--- prod.sh
+++ prod.sh
@@ -9,4 +9,7 @@ git pull
 # pkill -n npm
 
 npm run build
+sudo cp -r /home/ssda/git/web_afrita/public/ /var/www/ifritah/
+sudo cp -r /home/ssda/git/web_afrita/.next/* /var/www/ifritah/
+sudo chmod -R 755 /var/www/ifritah/
 npm run start | adddate
