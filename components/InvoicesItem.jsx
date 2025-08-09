9c17b18fad82b19610fdc19bfa71138739ba998d
ssda <abdulmohsen.jawad.almajed@gmail.com>
2025-08-09 18:16:59 +0000

    oh the issue was here I am smart xD

--- components/InvoicesItem.jsx
+++ components/InvoicesItem.jsx
@@ -96,7 +96,7 @@ const InvoicesItem = ({
           className={`${isOpenActionsList ? "flex" : "hidden"}  actions absolute text-sm text-app-gray py-2 flex-col w-max rounded-md bg-white top-8 left-8 shadow-lg shadow-primary/30 border border-primary/5`}
         >
           <Link
-            href={`/dashboard/bill/${id}`}
+            href={`/bill/${id}`}
             className="flex flex-row justify-center items-center gap-2 px-4 py-1 hover:bg-app-light-gray"
           >
             <HiOutlinePencil className="text-green-500" />
