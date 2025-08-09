63c5491cbc1c6a89bda0bb2ca35e59513e11d3cc
ssda <abdulmohsen.jawad.almajed@gmail.com>
2025-08-09 10:11:51 +0000

    add preview in this page lets see what will happen

--- app/dashboard/bill/[id]/page.jsx
+++ app/dashboard/bill/[id]/page.jsx
@@ -1,10 +1,13 @@
+import { InvoivePreview } from "@/components";
 import Skeleton from "@/components/Skeleton";
 
-export default function Orders() {
-  return (
-    <section id="orders" className="h-full">
-      <h1 className="mx-auto my-4">الطلبات</h1>
-      <Skeleton />
-    </section>
-  );
+export default function Orders({ params }) {
+	const { id } = params;
+	return (
+		<section id="orders" className="h-full">
+			<h1 className="mx-auto my-4">الطلبات</h1>
+			<Skeleton />
+			<InvoivePreview />
+		</section>
+	);
 }
