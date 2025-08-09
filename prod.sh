04010ec28bb3a92aa3a0597e22ccc35b9e25fbf0
ssda <abdh357@gmail.com>
2025-08-09 21:43:11 +0300

    more script

---
+++ prod.sh
@@ -0,0 +1,13 @@
+adddate() {
+    while IFS= read -r line; do
+        printf '%s %s\n' "$(date)" "$line";
+    done
+}
+
+git pull
+
+# pkill -n npm
+#
+# npm run build
+# npm run start | adddate
+ npm run dev | adddate 
