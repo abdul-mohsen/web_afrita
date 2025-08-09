b2971e734c91b0f4205ec965821e389154825222
ssda <abdulmohsen.jawad.almajed@gmail.com>
2025-08-09 18:05:05 +0000

    fix outer qr code with diffrenet way

--- components/InvoicesItem.jsx
+++ components/InvoicesItem.jsx
@@ -67,6 +67,7 @@ const InvoicesItem = ({
         <span className="item-number">
           {order < 10 ? `0${order + 1}` : order}
         </span>
+        <QRCodeComponent billId={data.id} />
         <h3 className="item-id text-primary font-bold ">{sequence_number}</h3>
         <span className="item-valyue mr-2  md:mr-0">
           {subtotal + vat - discount} ر.س
