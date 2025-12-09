fba07237241a12f7fbe0654f08f049882df6ea55
ssda <abdulmohsen.jawad.almajed@gmail.com>
2025-12-09 22:33:16 +0000

    fix: split the credit from the main code

---
+++ app/credit_bill/[id]/page.jsx
@@ -0,0 +1,41 @@
+"use client";
+
+import instance from "@/axios";
+import { InvoivePreview } from "@/components";
+import Skeleton from "@/components/Skeleton";
+import { useEffect, useState } from "react";
+
+export default function Orders({ params }) {
+  const [invoice, setInvoice] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchInvoices = async () => {
+      try {
+        const { id } = await params;
+        const response = await instance.get(`/api/v2/credit_bill/` + id, {});
+        console.log(response.data);
+        setInvoice(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching invoices:", error);
+        setError(error);
+        setLoading(false);
+      }
+    };
+    fetchInvoices();
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
+  console.log(invoice);
+
+  return (
+    <section id="orders" className="h-full">
+      <h1 className="mx-auto my-4">الطلبات</h1>
+      <Skeleton />
+      <InvoivePreview togglue={true} data={invoice} />
+    </section>
+  );
+}
