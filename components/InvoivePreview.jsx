abaec6252f093ba27c5be23bdb8cea8363ee6298
ssda <abdulmohsen.jawad.almajed@gmail.com>
2026-01-01 17:10:11 +0300

    fix: handle the string converting

--- components/InvoivePreview.jsx
+++ components/InvoivePreview.jsx
@@ -53,7 +53,10 @@ const InvoivePreview = ({ togglue, data }) => {
   }
 
   var combinedList = [...data.products, ...data.manual_products];
-  const maintenance_cost = round(parseFloat(data.maintenance_cost), 2);
+  const maintenance_cost = round(
+    parseFloat(parseFloat(data.maintenance_cost)),
+    2,
+  );
   if (maintenance_cost > 0) {
     const combinedList = [...combinedList, data.maintenance_cost];
   }
