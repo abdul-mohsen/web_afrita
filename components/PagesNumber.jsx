7400a668a728f14b921d900975be2aba588abe20
ssda <abdulmohsen.jawad.almajed@gmail.com>
2026-01-05 21:00:22 +0300

    fix: test number idk for pagnation

--- components/PagesNumber.jsx
+++ components/PagesNumber.jsx
@@ -1,39 +1,53 @@
 "use client";
-import React, { useState, useEffect } from "react";
-import { usePathname, useSearchParams } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
-export default function PagesNumber({ onPageChange }) {
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
-  const currentPage = parseInt(searchParams.get("page")) || 1;
+export default function Pagination({ onPageChange }) {
+  const router = useRouter();
+  const currentPage = parseInt(router.query.page) || 1; // Default to page 1
   const [selectedLink, setSelectedLink] = useState(currentPage);
 
   useEffect(() => {
-    setSelectedLink(currentPage);
+    setSelectedLink(currentPage); // Update local state when URL changes
   }, [currentPage]);
 
-  const updateQueryParams = (pageNumber) => {
-    const newParams = new URLSearchParams(searchParams.toString());
-    newParams.set("page", pageNumber);
-    window.history.pushState({}, "", `${pathname}?${newParams.toString()}`);
-  };
-
   const handleLinkClick = (pageNumber) => {
-    setSelectedLink(pageNumber);
-    updateQueryParams(pageNumber);
+    // Update the URL using the router
+    router.push({
+      pathname: router.pathname,
+      query: { ...router.query, page: pageNumber },
+    });
+
+    setSelectedLink(pageNumber); // Update local state
     onPageChange(); // Notify parent of the page change
   };
 
+  const handleNext = () => handleLinkClick(currentPage + 1); // Move to the next page
+  const handlePrev = () => {
+    if (currentPage > 1) {
+      handleLinkClick(currentPage - 1); // Move to the previous page
+    }
+  };
+
   return (
-    <div>
+    <div className="flex items-center gap-x-3">
+      <h5>الصفحة</h5>
       <button
-        onClick={() => handleLinkClick(currentPage - 1)}
+        className="py-1 px-2 text-xs rounded-md text-app-gray border-2"
+        onClick={handlePrev}
         disabled={currentPage === 1}
       >
         Prev
       </button>
-      <span>{currentPage}</span>
-      <button onClick={() => handleLinkClick(currentPage + 1)}>Next</button>
+
+      <span className="py-1 px-2 text-xs">{currentPage}</span>
+
+      <button
+        className="py-1 px-2 text-xs rounded-md text-app-gray border-2"
+        onClick={handleNext}
+      >
+        Next
+      </button>
     </div>
   );
 }
