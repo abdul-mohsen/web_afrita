a8d83a39d1c7f34fd102da950917ca91cef4eb5b
ssda <abdulmohsen.jawad.almajed@gmail.com>
2026-01-05 21:21:31 +0300

    fix: plz llm do it

--- components/PagesNumber.jsx
+++ components/PagesNumber.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 
-export default function Pagination({ onPageChange }) {
+export default function PagesNumber({ onPageChange }) {
   const router = useRouter();
   const [currentPage, setCurrentPage] = useState(1); // Default to page 1
 
