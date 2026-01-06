7ec064b7e6caa8a8c40312db86e0dc709399d4d6
ssda <abdulmohsen.jawad.almajed@gmail.com>
2026-01-06 21:27:35 +0300

    fix: gooo fix dev

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -34,7 +34,7 @@ const Nav = () => {
     if (searchParams.get("page")) {
       setQuery(searchParams.get("page"));
     }
-  })();
+  }, []); // Run effect when the query parameters change
 
   const updateQueryParams = (query) => {
     const newParams = new URLSearchParams(searchParams.toString());
