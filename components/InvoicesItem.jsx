bed6438d62087d08c0f51575763ef12bd8ca7e3f
ssda <abdulmohsen.jawad.almajed@gmail.com>
2026-01-23 13:10:52 +0300

    fix: the case check

--- components/InvoicesItem.jsx
+++ components/InvoicesItem.jsx
@@ -121,7 +121,7 @@ const InvoicesItem = ({
         <div
           className={`${isOpenActionsList ? "flex" : "hidden"}  actions absolute text-sm text-app-gray py-2 flex-col w-max rounded-md bg-white top-8 left-8 shadow-lg shadow-primary/30 border border-primary/5`}
         >
-          {state >= 0 ? (
+          {state !== 0 ? (
             <Link
               href={`/api/v2/${credit_state >= 1 ? "credit_bill_pdf" : "bill_pdf"}/${id}`}
               className="flex flex-row justify-center items-center gap-2 px-4 py-1 hover:bg-app-light-gray"
